c0f30db0a9a42c48e8464ede621711c4dab4911f
RAJ PATEL <patelraj24365@gmail.com>
2023-09-28 10:51:45 -0400

    json server added

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,42 @@
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
-    return ( 
-        <div className="home">
-            <h3>HomePage</h3>
-        </div>
-     );
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
+  useEffect(() => {
+    fetch('http://localhost:8000/blogs')
+    .then(res => {
+      if(!res.ok){
+        throw Error("Could not fetch the data");
+      }
+        return res.json();
+    })
+    .then(data => {
+        setBlogs(data);
+        setPending(false);
+        setError(null);
+    }).catch(err =>{
+      setPending(false);
+      setError(err.message);
+    })
+
+  }, []);
+
+  return (
+    <div className="home">
+      {error && <div>{ error }</div>}
+      {isPending && <div>Loading.....</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />}
+    </div>
+  );
 }
  
 export default Home;
